1bb5af3ff83f6871a06b44de84b750ae03ec43da
robinsonlin603 <93252094+robinsonlin603@users.noreply.github.com>
2022-01-11 11:18:36 +0800

    Add files via upload

---
+++ week3/Week_3_crawler.py
@@ -0,0 +1,13 @@
+import urllib.request as req
+import json
+url="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+with req.urlopen(url) as response:
+    data=json.load(response)
+datas=data["result"]["results"]
+with open("week3\data.csv","w",encoding="utf-8") as file:
+    for list in datas:
+        dist=list["address"][5:8]
+        pics=list["file"].lower()
+        pics=list["file"].split("http")
+        pic=pics[0]+"jpg"
+        file.write(list["stitle"]+","+dist+","+list["longitude"]+","+list["latitude"]+","+pic+"\n")
